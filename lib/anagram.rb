4ce4110a93c72f0c8e2605e7a4d7ddeb6a99f093
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-11-05 10:46:27 +1100

    complete lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(possible_anagrams)
+        result_array =[]
+        possible_anagrams.each do |possible_anagram|
+            result_array << possible_anagram if possible_anagram.chars.sort == @word.chars.sort 
+        end
+        result_array
+    end
+
+end
